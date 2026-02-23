63e6089bf7ee5436b162971a149efdede1fda27b
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-02-23 17:27:03 +0530

    Time: 0 ms (100%), Space: 9.8 MB (93.61%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool alphan(char c) {
+        if ((c >= '0' && c <= '9') ||
+            (tolower(c) >= 'a' && tolower(c) <= 'z')) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st = 0, end = s.length() - 1;
+
+        while (st < end) {
+            if (!alphan(s[st])) {
+                st++;
+                continue;
+            }
+            if (!alphan(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
