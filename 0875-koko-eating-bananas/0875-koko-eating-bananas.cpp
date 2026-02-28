29163f6062ab93cfc9efcebf99a5b5aad055c3db
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-02-28 21:32:23 +0530

    Time: 10 ms (39.53%), Space: 24.1 MB (5.08%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,42 @@
+int fmax(vector<int> arr) {
+    int mx = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        mx = max(mx, arr[i]);
+    }
+    return mx;
+}
+
+long long hour(vector<int>& arr, int mid) {
+    long long thr = 0;
+    int m = arr.size();
+
+    for (int i = 0; i < m; i++) {
+        thr += (long long)ceil((double)arr[i] / (double)mid);
+    }
+    return thr;
+}
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int st = 1;
+        int m = piles.size();
+        long long end = fmax(piles); // maximum element
+        int ans = 0;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            long long thr =
+                hour(piles, mid); //(banana per hour(banana/hour))*total hour
+
+            if (thr <= h) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
