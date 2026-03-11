99ddb42a5324259942055a29775d56ac18875ec6
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-03-11 17:37:43 +0530

    Time: 26 ms (78.05%), Space: 60.2 MB (91.5%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,25 +5,21 @@ public:
             return 0;
         }
 
-        int n = nums.size();
-        int lng = 1;
+        sort(nums.begin(), nums.end());
+        int maxi = 0;
+        int cnt = 0;
 
-        unordered_set<int> st;
-        for (int i = 0; i < n; i++) {
-            st.insert(nums[i]);
-        }
-        
-        for (auto v : st) {
-            if (st.find(v - 1) == st.end()) {
-                int cnt = 1;
-                int x = v;
-                while (st.find(x + 1) != st.end()) {
-                    x++;
-                    cnt++;
-                }
-                lng = max(lng, cnt);
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                continue;
+            } else if (nums[i] + 1 == nums[i + 1]) {
+                cnt++;
+            } else {
+                maxi = max(maxi, cnt);
+                cnt = 0;
             }
         }
-        return lng;
+        maxi = max(maxi, cnt);
+        return maxi + 1;
     }
 };
