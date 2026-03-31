fc6592acab0e4b340cc3017e11fac304f574a115
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-03-31 07:08:41 +0530

    Time: 0 ms (100%), Space: 50.4 MB (69.64%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
+    int midx(vector<vector<int>>& mat, int n, int col) {
+        int m = -1;
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > m) {
+                m = mat[i][col];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                bool ip = true;
+        int low = 0, high = m - 1;
+        while (low <= high) {
 
-                if (i > 0 && mat[i][j] <= mat[i - 1][j]) {
-                    ip = false;
-                }
-                if (i < n - 1 && mat[i][j] <= mat[i + 1][j]) {
-                    ip = false;
-                }
-                if (j > 0 && mat[i][j] <= mat[i][j - 1]) {
-                    ip = false;
-                }
-                if (j < m - 1 && mat[i][j] <= mat[i][j + 1]) {
-                    ip = false;
-                }
+            int mid = (low + high) / 2;
+            int mi = midx(mat, n, mid);
 
-                if (ip) {
-                    return {i, j};
-                }
+            int left = mid - 1 >= 0 ? mat[mi][mid - 1] : -1;
+            int right = mid + 1 < m ? mat[mi][mid + 1] : -1;
+
+            if (mat[mi][mid] > left && mat[mi][mid] > right) {
+                return {mi, mid};
+
+            } else if (mat[mi][mid] < left) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return {-1, -1};
