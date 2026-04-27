3f8b1d867994a013890d1c7676c654e91bfdf934
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-04-27 07:14:13 +0530

    Time: 3 ms (83.07%), Space: 19 MB (56.1%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    bool vl(string& s) {
+    bool doesAliceWin(string s) {
         for (auto c : s) {
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 return true;
             }
         }
         return false;
-    }
-    bool doesAliceWin(string s) {
-        if (!vl(s)) {
-            return false;
-        }
-        return true;
     }
 };
