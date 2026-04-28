13425be07d3ed7caec1ce46761e1e80fdb5d6dcb
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-04-28 18:01:15 +0530

    Time: 0 ms (100%), Space: 9 MB (69.34%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                cnt++;
+            }
+            if (cnt == k) {
+                break;
+            }
+            ans += s[i];
+        }
+        return ans;
+    }
+};
