076f2cc692b074471fe04d0aa1e0174789497cab
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-05-20 16:54:12 +0530

    Time: 60 ms (37.18%), Space: 24 MB (24.03%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,22 +8,42 @@
  */
 class Solution {
 public:
+    ListNode* cp(ListNode* t1, ListNode* t2, int d) {
+
+        while (d) {
+            d--;
+            t2 = t2->next;
+        }
+
+        while (t1 != t2) {
+            t1 = t1->next;
+            t2 = t2->next;
+        }
+        return t1;
+    }
     ListNode* getIntersectionNode(ListNode* head1, ListNode* head2) {
         unordered_map<ListNode*, int> mpp;
 
-        ListNode* temp = head1;
+        ListNode* temp1 = head1;
+        ListNode* temp2 = head2;
 
-        while (temp != NULL) {
-            mpp[temp] = 1;
-            temp = temp->next;
+        int n1 = 0;
+        int n2 = 0;
+
+        while (temp1 != NULL) {
+            n1++;
+            temp1 = temp1->next;
         }
 
-        temp = head2;
-        while (temp != NULL) {
-            if (mpp.find(temp) != mpp.end()) {
-                return temp;
-            }
-            temp = temp->next;
+        while (temp2 != NULL) {
+            n2++;
+            temp2 = temp2->next;
+        }
+
+        if (n1 < n2) {
+            return cp(head1, head2, n2 - n1);
+        } else {
+            return cp(head2, head1, n1 - n2);
         }
         return NULL;
     }
