fb06b54d940c73dc218e9ef2b907f80dd9713427
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-05-23 17:14:47 +0530

    Time: 0 ms (100%), Space: 8.3 MB (57.86%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool checkString(string s) {
-        return (s.find("ba") == std::string::npos);
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i - 1] == 'b' && s[i] == 'a') {
+                return false;
+            }
+        }
+        return true;
     }
 };
