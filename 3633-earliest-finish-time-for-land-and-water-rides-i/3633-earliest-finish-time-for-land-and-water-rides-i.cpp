6b92b3001ed7579c6fe3949cae7b8e5ee6ae5988
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-06-03 10:36:05 +0530

    Time: 33 ms (5.33%), Space: 92 MB (13.93%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& lst, vector<int>& ldr, vector<int>& wst, vector<int>& wdr) {
-        int ans = INT_MAX;
-        for (int i = 0; i < lst.size(); i++) {
-            for (int j = 0; j < wst.size(); j++) {
-                int ls = lst[i];  // land open
-                int ld = ldr[i];  // land duration
-                int lf = ls + ld; // land finish
+    int earliestFinishTime(vector<int>& ls, vector<int>& ld, vector<int>& ws, vector<int>& wd) {
+        int ans = 1e9;
+        for (int i = 0; i < ls.size(); i++) {
+            for (int j = 0; j < ws.size(); j++) {
+                int lw = ls[i] + ld[i];
+                lw = max(lw, ws[j]) + wd[j];
 
-                int ws = wst[j];       // water open
-                int wd = wdr[j];       // water duration
-                int was = max(ws, lf); // wait if not open
-                int f1 = was + wd;     // total finish {land->water}
+                int wl = ws[j] + wd[j];
+                wl = max(wl, ls[i]) + ld[i];
 
-                int wf = ws + wd;      // water finish
-                int las = max(ls, wf); // wait if not open
-                int f2 = las + ld;     // total finish {water->land}
-
-                ans = min(ans, min(f1, f2));
+                ans = min({ans, lw, wl});
             }
         }
         return ans;
