907eab979c7b5e701b844df5de5ba9c0a4966b8b
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-06-26 20:51:41 +0530

    Time: 60 ms (60.93%), Space: 120.9 MB (44.37%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int p = n + 1;
-        long long cnt = 0;
-
-        vector<int> freq(2 * n + 2);
-        vector<int> pre(2 * n + 2);
-
-        freq[p] = 1;
-        pre[p] = 1;
+        unordered_map<int, long long> mpp;
+        long long ans = 0, sum = 0, cnt = 0;
+        mpp[0] = 1;
 
         for (auto& i : nums) {
-            p += (i == target ? 1 : -1);
-            freq[p]++;
-
-            pre[p] = pre[p - 1] + freq[p];
-            cnt += pre[p - 1];
+            if (i == target) {
+                cnt += mpp[sum];
+                sum++;
+            } else {
+                sum--;
+                cnt -= mpp[sum];
+            }
+            mpp[sum]++;
+            ans += cnt;
         }
-        return cnt;
+        return ans;
     }
 };
