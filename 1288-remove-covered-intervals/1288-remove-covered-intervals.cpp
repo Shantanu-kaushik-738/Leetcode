9400d7e19be39dead36eb84c06f8abf7bbed432a
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-07-06 07:31:35 +0530

    Time: 4 ms (28.48%), Space: 15.2 MB (60.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& inter) {
+        sort(begin(inter), end(inter), [](auto& a, auto& b) {
+            if (a[0] == b[0])
+                return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        int cnt = 0, p = 0;
+
+        for (auto& i : inter) {
+            if (i[1] > p) {
+                cnt++;
+                p = i[1];
+            }
+        }
+        return cnt;
+    }
+};
