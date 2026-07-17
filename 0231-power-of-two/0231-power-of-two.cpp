9cc66edd73b20ead5bdc0a3941c369910fd05677
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-07-17 09:55:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return (n > 0) && ((n & (n - 1)) == 0); //
+        return (n > 0) && ((n & (n - 1)) == 0); // power of 2 has only one set of bit
     }
 };
