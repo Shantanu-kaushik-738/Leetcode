be11f773de7d4462cbb476a9c063f02a4c3b376b
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-07-30 14:52:50 +0530

    Time: 0 ms (100%), Space: 28.4 MB (64.8%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> res(n);
         for (int i = 2 * n - 1; i >= 0; i--) {
             while (!s.empty() && s.top() <= nums[i % n]) s.pop();
-            if (i < n) res[i] = s.empty() ? -1 : s.top();
+            res[i % n] = s.empty() ? -1 : s.top();
             s.push(nums[i % n]);
         }
         return res;
