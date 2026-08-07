64e3ba8dab65fadf9a649c05cae22fca44a04c11
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-08-07 19:47:37 +0530

    Time: 16 ms (85.17%), Space: 139.1 MB (91.83%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,17 +4,13 @@ public:
         vector<int> res;
         deque<int> dq;
 
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (!dq.empty() && dq.front() <= i - k) dq.pop_front(); // outside the k window
+            
             while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
             dq.push_back(i);
+            if (i >= k - 1) res.push_back(nums[dq.front()]);
         }
-        for (int i = k; i < nums.size(); i++) {
-            res.push_back(nums[dq.front()]);
-            while (!dq.empty() && dq.front() <= i - k) dq.pop_front();
-            while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
-            dq.push_back(i);
-        }
-        res.push_back(nums[dq.front()]);
         return res;
     }
 };
