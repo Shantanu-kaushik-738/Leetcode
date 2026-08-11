5320399be2b17a939265d271f1d7e120a70a0169
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-08-11 22:33:01 +0530

    Time: 0 ms (100%), Space: 10.3 MB (85.61%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,19 +1,33 @@
+// class Solution {
+// public:
+//     int timeRequiredToBuy(vector<int>& tickets, int k) {
+//         queue<pair<int, int>> q;
+//         int res = 0;
+
+//         for (int i = 0; i < tickets.size(); i++) q.push({tickets[i], i});
+
+//         while (!q.empty()) {
+//             q.front().first--;
+//             res++;
+
+//             if (q.front().first > 0) q.push(q.front()); // tickets remains
+//             if (q.front().second == k && q.front().first == 0) return res; // (k person && buy all tickets)
+
+//             q.pop();
+//         }
+//         return res;
+//     }
+// };
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<pair<int,int>>q;
         int res = 0;
-
-        for(int i=0; i<tickets.size(); i++) q.push({tickets[i], i});
-
-        while (!q.empty()) {
-            q.front().first--;
-            res++;
-
-            if (q.front().first > 0) q.push(q.front()); // tickets remains
-            if (q.front().second == k && q.front().first == 0) return res; // (k person && buy all tickets)
-
-            q.pop();
+        for (int i = 0; i < tickets.size(); i++) {
+            if (i <= k) {
+                res += min(tickets[i], tickets[k]);
+            } else {
+                res += min(tickets[i], tickets[k] - 1);
+            }
         }
         return res;
     }
