df262495223b812aa60cf782fdc08a1b62f831e9
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-08-18 07:25:07 +0530

    Time: 11 ms (22.18%), Space: 32.5 MB (28.95%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        unordered_map<int, unordered_set<int>> mp;
+        unordered_map<int, int> frq;
         for (int i = 0; i <= nums.size() - k; i++) {
+            unordered_map<int, bool> sn;
             for (int j = i; j < i + k; j++) {
-                mp[nums[j]].insert(i);
+                if (!sn[nums[j]]) {
+                    frq[nums[j]]++;
+                    sn[nums[j]] = true;
+                }
             }
         }
 
         int res = -1;
-        for (auto& i : mp) {
-            if (i.second.size() == 1) res = max(res, i.first);
+        for (auto& p : frq) {
+            if (p.second == 1) res = max(res, p.first);
         }
         return res;
     }
