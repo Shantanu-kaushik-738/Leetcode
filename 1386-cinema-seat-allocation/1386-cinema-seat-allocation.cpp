8fbe09928112d605c5d716094a07bedf44239b96
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-08-19 15:52:46 +0530

    Time: 15 ms (89.19%), Space: 60.1 MB (73.48%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& rev) {
-        unordered_map<int, unordered_set<int>> mp; // row -> booked seat in row
+        unordered_map<int, int> mp; // row -> booked seat in row
 
-        for (auto& i : rev) mp[i[0]].insert(i[1]);
+        for (auto& i : rev) mp[i[0]] |= (1 << i[1]); // or opration 
 
         int res = (n - mp.size()) * 2; // empty seat
 
-        for (auto& i : mp) {
-            auto avl = [&](int p) { // lambda funx to check seat p is avaiable or not
-                return i.second.find(p) == i.second.end();
-            };
+        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+        int maskB = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+        int maskC = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
 
-            bool A = avl(2) && avl(3) && avl(4) && avl(5);
-            bool B = avl(4) && avl(5) && avl(6) && avl(7);
-            bool C = avl(6) && avl(7) && avl(8) && avl(9);
+        for (auto& i : mp) {
+            
+            bool A = (maskA & i.second) == 0;
+            bool B = (maskB & i.second) == 0;
+            bool C = (maskC & i.second) == 0;
 
             if (A && C) res += 2;
             else if (A || B || C) res++;
