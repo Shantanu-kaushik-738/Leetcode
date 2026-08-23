cd4397d9d87400fc074d5fa1df4ecdd7e1f76f3f
Shantanu_kaushik_ <kaushikshantanu420@gmail.com>
2026-08-23 10:38:04 +0530

    Time: 93 ms (60.4%), Space: 242.1 MB (93.54%) - LeetHub

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int l, int u) {
-        vector<vector<int>> res;
-
-        sort(begin(nums), end(nums));
-        nums.erase(unique(begin(nums), end(nums)), end(nums)); // remove duplicate el from nums
-
-        vector<int> num;
-        for (auto& i : nums) {
-            if (i >= l && i <= u) num.push_back(i);
+    vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
+        nums.push_back(lower - 1);
+        nums.push_back(upper + 1);
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < lower || nums[i - 1] > upper) continue;
+            if (nums[i] - nums[i - 1] - 1 > 0) ans.push_back({nums[i - 1] + 1, nums[i] - 1});
         }
-
-        num.insert(begin(num), l - 1);
-        num.push_back(u + 1);
-
-        for (int i = 1; i < num.size(); i++) {
-            if ((num[i - 1] + 1) <= (num[i] - 1)) res.push_back({(num[i - 1] + 1), (num[i] - 1)});
-        }
-        return res;
+        return ans;
     }
 };
